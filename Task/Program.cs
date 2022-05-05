95c635aed2bc9b28190e0ef62422dafcaae097fc
Терехин Максим Александрович <MTerekhin@synergy.ru>
2022-05-05 09:05:32 +0300

    Создал цикл, который берет случаное количество символов в диапазоне от 0 до 3 и выводит значения массиво со случайными индексами

--- Task/Program.cs
+++ Task/Program.cs
@@ -2,7 +2,7 @@
 {
     for(int i = 0; i < mass.Length; i++)
     {
-        Console.Write($"Введите {i}-е значение в массив");
+        Console.Write($"Введите {i}-е значение в массив: ");
         mass[i] = Console.ReadLine();
     }
 }
@@ -15,5 +15,26 @@ void PrintArray(string[] mass)
     }
 }
 
+Console.Write("Введите длину массива: ");
 int len = Convert.ToInt32(Console.ReadLine());
 string[] array = new string[len];
+
+CreateArray(array);
+PrintArray(array);
+Console.WriteLine();
+
+int k = -1;
+int j = 0;
+int n = new Random().Next(0, 4);
+
+while(j < n)
+{
+    int m = new Random().Next(0, len);
+    if(m != k)
+    {
+        k = m;
+        Console.Write(array[m] +"; ");
+        j++;
+
+    }
+}
