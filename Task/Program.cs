95e3fab33b6f6263dcb5ff6014f1a9d6a111d55d
Терехин Максим Александрович <MTerekhin@synergy.ru>
2022-05-05 08:24:15 +0300

    Создал переменные одна из которых задаёт длину массива, а другая сам массив

--- Task/Program.cs
+++ Task/Program.cs
@@ -1,2 +1,4 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿
+
+int len = Convert.ToInt32(Console.ReadLine());
+string[] array = new string[len];
