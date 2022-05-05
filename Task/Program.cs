d4f970241ca3de831cbac615898ba773e3dd074e
Терехин Максим Александрович <MTerekhin@synergy.ru>
2022-05-05 08:34:37 +0300

    Создал метод, где пользователь будет задавать массив самостоятельно через консоль

--- Task/Program.cs
+++ Task/Program.cs
@@ -1,4 +1,12 @@
-﻿
+﻿void CreateArray(string[] mass)
+{
+    for(int i = 0; i < mass.Length; i++)
+    {
+        Console.Write($"Введите {i}-е значение в массив");
+        mass[i] = Console.ReadLine();
+    }
+}
+
 
 int len = Convert.ToInt32(Console.ReadLine());
 string[] array = new string[len];
